5a3ca2b0bf05500406175314d5cfe1fa5d807afa
Potato <jameslovejamesjj@gmail.com>
2023-06-14 14:40:17 +0800

    主頁面更新與按鈕處理

---
+++ CoreMVC5_UsedBookProject/wwwroot/js/buttons.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    var buttonpartial = $('.button-cover');
+    var btnLogin = buttonpartial.find('.btn-login');
+    var btnLogout = buttonpartial.find('.btn-logout');
+    var interval;
+
+    buttonpartial.hover(
+        function () {
+            clearInterval(interval);
+            var self = this;
+            interval = setInterval(function () {
+                $(self).addClass('hovered');
+                btnLogin.stop().animate({ opacity: 1 }, 1000);
+                btnLogout.stop().animate({ opacity: 1 }, 1000);
+                clearInterval(interval);
+            }, 1500);
+        },
+        function () {
+            clearInterval(interval);
+            buttonpartial.removeClass('hovered');
+            btnLogin.stop().animate({ opacity: 0 }, 500);
+            btnLogout.stop().animate({ opacity: 0 }, 500);
+        }
+    );
+});
