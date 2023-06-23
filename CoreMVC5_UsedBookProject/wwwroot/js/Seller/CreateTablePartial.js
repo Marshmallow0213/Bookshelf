aff29754913efd57d454643cb6050ec93ed6d521
samchang1221 <samchang1221@outlook.com>
2023-06-23 23:14:42 +0800

    login fitcontent

--- CoreMVC5_UsedBookProject/wwwroot/js/Seller/CreateTablePartial.js
+++ CoreMVC5_UsedBookProject/wwwroot/js/Seller/CreateTablePartial.js
@@ -34,37 +34,29 @@ function ShowLength(element) {
 //end
 //start check img value valid
 for (let i = 1; i <= 9; i++) {
-    let preImg = document.getElementById(`Image${i}`).value;
-    let preId = document.getElementById(`ProductId`).value;
-    if (preImg != "無圖片") {
-        document.getElementById(`img${i}Div`).innerHTML = `<img src='../Images/Products/${preId}/${preImg}' alt='img${i}' id='img${i}' onload='testLoad(${i})' onerror='testError(${i})'>`;
-    }
     document.getElementById(`file${i}`).addEventListener("click", () => {
         document.getElementById(`p-file${i}`).innerText = "未選擇任何檔案";
         let preImg = document.getElementById(`Image${i}`).value;
-        let preId = document.getElementById(`ProductId`).value;
         if (preImg != "無圖片") {
-            document.getElementById(`img${i}Div`).innerHTML = `<img src='../Images/Products/${preId}/${preImg}' alt='img${i}' id='img${i}' onload='testLoad(${i})' onerror='testError(${i})'>`;
+            document.getElementById(`img${i}Div`).innerHTML = `<div class="m-auto"><i class="bi bi-images text-center m-auto d-block" style="font-size: 50px;"></i><p>No image</p></div>`;
         }
     });
     document.getElementById(`file${i}`).addEventListener("input", () => {
         let file = document.getElementById(`file${i}`);
         let p_file = document.getElementById(`p-file${i}`);
-        if (file.files[0].size > 2097152 ) {
+        if (file.files[0].size > 2097152) {
             alert("檔案過大!");
             file.value = "";
             p_file.innerText = "未選擇任何檔案";
-            let preImg = document.getElementById(`Image${i}`).value;
             let preId = document.getElementById(`ProductId`).value;
-            document.getElementById(`img${i}Div`).innerHTML = `<img src='../Images/Products/${preId}/${preImg}' alt='img${i}' id='img${i}' onload='testLoad(${i})' onerror='testError(${i})'>`;
+            document.getElementById(`img${i}Div`).innerHTML = `<div class="m-auto"><i class="bi bi-images text-center m-auto d-block" style="font-size: 50px;"></i><p>No image</p></div>`;
         }
         else if (!file.files[0].name.match(/\.(jpg|jpeg|png|gif)$/i)) {
             alert('不是正確圖檔!');
             file.value = "";
             p_file.innerText = "未選擇任何檔案";
-            let preImg = document.getElementById(`Image${i}`).value;
             let preId = document.getElementById(`ProductId`).value;
-            document.getElementById(`img${i}Div`).innerHTML = `<img src='../Images/Products/${preId}/${preImg}' alt='img${i}' id='img${i}' onload='testLoad(${i})' onerror='testError(${i})'>`;
+            document.getElementById(`img${i}Div`).innerHTML = `<div class="m-auto"><i class="bi bi-images text-center m-auto d-block" style="font-size: 50px;"></i><p>No image</p></div>`;
         }
         else {
             p_file.innerText = file.files[0].name;
