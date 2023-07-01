089aec36a37fdc592927dea54511db1521d88eb3
劉威志 <wen841687@gmail.com>
2023-07-01 23:17:35 +0800

    刪除一些不必要的

--- CoreMVC5_UsedBookProject/Controllers/HomeController.cs
+++ CoreMVC5_UsedBookProject/Controllers/HomeController.cs
@@ -50,7 +50,7 @@ namespace CoreMVC5_UsedBookProject.Controllers
             int targetLength = target.Length;
             int[,] distanceMatrix = new int[sourceLength + 1, targetLength + 1];
 
-            // 初始化距离矩阵
+            
             for (int i = 0; i <= sourceLength; i++)
             {
                 distanceMatrix[i, 0] = i;
@@ -61,7 +61,6 @@ namespace CoreMVC5_UsedBookProject.Controllers
                 distanceMatrix[0, j] = j;
             }
 
-            // 计算编辑距离
             for (int i = 1; i <= sourceLength; i++)
             {
                 for (int j = 1; j <= targetLength; j++)
@@ -73,10 +72,9 @@ namespace CoreMVC5_UsedBookProject.Controllers
                 }
             }
 
-            // 判断是否匹配成功
             int maxEditDistance = Math.Max(sourceLength, targetLength);
             int normalizedEditDistance = distanceMatrix[sourceLength, targetLength] * 100 / maxEditDistance;
-            double similarityThreshold = 0.8; // 设置相似度阈值
+            double similarityThreshold = 0.8; 
             return normalizedEditDistance <= (1 - similarityThreshold) * 100;
         }
         public IActionResult SearchProductbyName(string name)
@@ -137,40 +135,6 @@ namespace CoreMVC5_UsedBookProject.Controllers
 
 
 
-            // 判断集合是否有数据
-            //if (products.Count == 0 )
-            //{
-            //    // 进行模糊匹配
-            //    var _products = _context.Products
-            //        .Where(p => FuzzyMatch(p.Title, name))
-            //        .Select(p => new ProductViewModel
-            //        {
-            //            ProductId = p.ProductId,
-            //            Title = p.Title,
-            //            ISBN = p.ISBN,
-            //            Author = p.Author,
-            //            Publisher = p.Publisher,
-            //            PublicationDate = p.PublicationDate,
-            //            Degree = p.Degree,
-            //            ContentText = p.ContentText,
-            //            Image1 = p.Image1,
-            //            Image2 = p.Image2,
-            //            Status = p.Status,
-            //            Trade = p.Trade,
-            //            UnitPrice = p.UnitPrice,
-            //            CreateDate = p.CreateDate,
-            //            EditDate = p.EditDate,
-            //            CreateBy = p.CreateBy
-            //        })
-            //        .ToList();
-            //    MyProductsViewModel mysearchproductmodel = new MyProductsViewModel
-            //    {
-            //        Products = _products
-            //    };
-
-            //    // 指派使用ListTable.cshtml
-            //    return View(mysearchproductmodel);
-            //}
             ViewBag.Count = $"find {products.Count} products";
             MyProductsViewModel mymodel = new MyProductsViewModel
             {
