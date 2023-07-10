a6695af5f94de161fb6a64e59799809c61d971c8
劉威志 <wen841687@gmail.com>
2023-07-10 11:54:44 +0800

    文字修改

--- CoreMVC5_UsedBookProject/Migrations/AdminAccount/20230704055952_init.Designer.cs
+++ CoreMVC5_UsedBookProject/Migrations/AdminAccount/20230704055952_init.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoreMVC5_UsedBookProject.Migrations.AdminAccount
 {
     [DbContext(typeof(AdminAccountContext))]
-    [Migration("20230704055628_init")]
+    [Migration("20230704055952_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
