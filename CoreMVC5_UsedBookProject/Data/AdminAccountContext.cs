0ea83432f0f642564c19d1bc796ce500625ec228
samchang1221 <samchang1221@outlook.com>
2023-07-11 14:56:20 +0800

    Deafult.jpg

--- CoreMVC5_UsedBookProject/Data/AdminAccountContext.cs
+++ CoreMVC5_UsedBookProject/Data/AdminAccountContext.cs
@@ -55,7 +55,7 @@ namespace CoreMVC5_UsedBookProject.Data
                 );
             modelBuilder.Entity<Textbox>()
                 .HasData(
-                new Textbox { Id = "T001", TextValue = "預設文字", Image1 = "Error.png", Image2 = "Error.png", Image3 = "Error.png" }
+                new Textbox { Id = "T001", TextValue = "預設文字", Image1 = "Deafult.jpg", Image2 = "Deafult.jpg", Image3 = "Deafult.jpg" }
                 );
         }
     }
