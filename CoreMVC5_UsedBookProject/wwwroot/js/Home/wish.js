c88a55efec5ea0f2dc4b31fea6bab1eff22239ab
劉威志 <wen841687@gmail.com>
2023-07-19 23:33:10 +0800

    book api

--- CoreMVC5_UsedBookProject/wwwroot/js/Home/wish.js
+++ CoreMVC5_UsedBookProject/wwwroot/js/Home/wish.js
@@ -1,23 +1,19 @@
-﻿async function searchByISBN() {
+﻿function searchByISBN() {
     const apiKey = "AIzaSyAeJpJzP9zX-Bz7P-qwFIwybZ7A92MIa0k"; // 替換為你的 Google Books API 金鑰
-    const isbn = document.getElementById("isbnInput").value;
+    const isbn = document.getElementById("ISBN").value;
 
-    try {
-        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apiKey}`;
-
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        if (response.ok) {
-            displayResult(data);
-        } else {
-            console.error("查詢失敗，HTTP 響應碼：" + response.status);
-        }
-    } catch (error) {
-        console.error("發生錯誤：" + error.message);
+    if (!isbn) {
+        alert("ISBN 號碼不能為空");
+        return;
     }
-}
 
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => displayResult(data))
+        .catch(error => console.error("發生錯誤：" + error.message));
+}
 function displayResult(data) {
     const resultContainer = document.getElementById("resultContainer");
     resultContainer.innerHTML = "";
@@ -25,6 +21,8 @@ function displayResult(data) {
     if (data.totalItems > 0) {
         const bookInfo = data.items[0].volumeInfo;
         const title = bookInfo.title;
+        let titleInput = document.getElementById("Title");
+        titleInput.value = title;
         const authors = bookInfo.authors ? bookInfo.authors.join(", ") : "未知作者";
         const description = bookInfo.description || "無描述";
         const thumbnail = bookInfo.imageLinks ? bookInfo.imageLinks.thumbnail : "";
@@ -37,7 +35,17 @@ function displayResult(data) {
                 `;
 
         resultContainer.innerHTML = bookDetails;
+        Swal.fire(
+            '找到書籍',
+            '',
+            'success'
+        )
     } else {
         resultContainer.innerHTML = "找不到符合的書籍";
+        Swal.fire(
+            '查無此書，請手動輸入',
+            '',
+            'error'
+        )
     }
 }
